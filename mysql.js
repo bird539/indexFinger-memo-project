26e3bd1ef8db4fa8a704883a846d4abf28a655ad
bird539 <bird5397@gmail.com>
2022-09-04 16:38:26 +0900

    mysql experience

--- mysql.js
+++ mysql.js
@@ -1,14 +1,52 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-    host: ''%'',    // 호스트 주소
-    user: 'test',           // mysql user
+    host: 'localhost',    // 호스트 주소
+    user: 'root',           // mysql user
     password: 'bird553399',       // mysql password
-    database: 'testDB',         // mysql 데이터베이스
+    database: 'o2',         // mysql 데이터베이스
 });
 connection.connect();
-connection.query('SELECT 1 + 1 AS solution',
-    function (error, results, fields) {
-        if (error) throw error;
-        console.log('The solution is: ', results[0].solution);
-    });
+var sql = 'SELECT * FROM topic';
+// connection.query(sql, function (err, rows, fields) {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         for(var i=0; i<rows.length; i++){
+//             console.log(rows[i].description);
+//         }
+//     }
+// });
+/*
+//추가
+var sql = 'INSERT INTO topic (title, description, author) VALUES(?, ?, ?)';
+var params = ['Super', 'Watcher', 'banksy'];
+connection.query(sql, params, function(err, rows, fields){
+    if(err){
+        console.log(err);
+    }else{
+        console.log(rows);
+    }
+});
+
+//업데이트
+var sql = 'UPDATE topic SET title=?, author=? WHERE id=?';
+var params = ['NPM', 'bird',1];
+connection.query(sql, params, function(err, rows, fields){
+    if(err){
+        console.log(err);
+    }else{
+        console.log(rows);
+    }
+});
+*/
+//삭제
+var sql = 'DELETE FROM topic WHERE id=?';
+var params = [1];
+connection.query(sql, params, function(err, rows, fields){
+    if(err){
+        console.log(err);
+    }else{
+        console.log(rows);
+    }
+});
 connection.end();
